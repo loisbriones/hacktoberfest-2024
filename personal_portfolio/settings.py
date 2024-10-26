b6400d9ac753a1f1118640274cd1e4e5db88c245
JcAvm <j.calonso@udc.es>
2024-10-26 16:32:53 +0200

    Añadido gitignore, cambios no relevantes en setting y cambio en home.html

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,15 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g#w-9x^nk9kk38tdifkzi2o!1i1rp$$f3d6-4+%rul81*u5f32'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost:8000","127.0.0.1"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig',
+    'pages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
