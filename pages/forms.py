538fc254cc3103ba39e11979777cdd169f388e0d
loisbriones <lois.briones@udc.es>
2024-10-26 16:23:49 +0200

    Formulario añadido + html mejorado + correcion de conflictos

---
+++ pages/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Proyecto, Experiencia, Habilidad, Lenguaje, Social, Trabajo, Usuario
+
+class ProyectoForm(forms.ModelForm):
+    class Meta:
+        model = Proyecto
+        fields = ['nombre', 'descripcion']
+
+class ExperienciaForm(forms.ModelForm):
+    class Meta:
+        model = Experiencia
+        fields = ['nombre', 'listProyecto']
+
+class HabilidadForm(forms.ModelForm):
+    class Meta:
+        model = Habilidad
+        fields = ['nombre', 'descripcion']
+
+class LenguajeForm(forms.ModelForm):
+    class Meta:
+        model = Lenguaje
+        fields = ['nombre', 'expirencia']
+
+class SocialForm(forms.ModelForm):
+    class Meta:
+        model = Social
+        fields = ['nombre', 'url']
+
+class TrabajoForm(forms.ModelForm):
+    class Meta:
+        model = Trabajo
+        fields = ['nombre', 'descripcion', 'fechaIni', 'fechaFin']
+
+class UsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = [
+            'nombre', 'descripcion', 'nivel', 'imagen',
+            'listHabilidad', 'listaLenguaje', 'listaTrabajo',
+            'listaSocial', 'listaProyecto', 'listaExperiencia'
+        ]
+
